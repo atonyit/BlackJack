a4386a1957e206919a83bbd0284f8d45ead7f49f
Alan <alanitoyah96@gmail.com>
2024-05-11 21:40:36 -0500

    Styled button, linked onlick function to js and moved previous conditional in js to startGame function

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ let isAlive = true
 
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21){
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+function startGame(){
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    console.log(message)
 }
-
-//CASH OUT
-console.log(message)
 
 
 
