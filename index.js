5279a86df4adaa18a05700c3aeb2b0fead5b1125
Alan <alanitoyah96@gmail.com>
2024-05-11 22:07:37 -0500

    js changes to update words in html

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
-let message = ""
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21){
-        message = "Wohoo! You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
-    console.log(message)
+    messageEl.textContent = message;
 }
 
 
